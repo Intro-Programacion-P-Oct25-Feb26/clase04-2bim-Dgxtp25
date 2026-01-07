5010610a62848b2821fe84c79eb4de7070af092b
UTPL <dgxtpbr@gmail.com>
2026-01-07 12:19:12 -0500

    Cambio 1, Cambio2 y Cambio 3

--- ejemplos/Ejemplos/src/ejemplos03/Ejemplo06.java
+++ ejemplos/Ejemplos/src/ejemplos03/Ejemplo06.java
@@ -9,5 +9,22 @@ package ejemplos03;
  * @author UTPL
  */
 public class Ejemplo06 {
-    
+
+    static int suma = 0;
+
+    public static void main(String[] args) {
+        System.out.printf("Valor de variable suma (main): %d%n", suma);
+        obtenerSuma(10, 30);
+        obtenerSumaDos();
+    }
+
+    public static void obtenerSuma(int a, int b) {
+        suma = a + b;
+        System.out.printf("Valor de variable suma (obtenerSuma): %d%n", suma);
+    }
+
+    public static void obtenerSumaDos() {
+        suma = suma + 100;
+        System.out.printf("Valor de variable sumaDos (obtenerSumaDos): %d%n", suma);
+    }
 }
